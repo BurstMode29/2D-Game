54d5781ef24e9df617527a870052579943641163
Rafeeq Weideman <106829867+BurstMode29@users.noreply.github.com>
2022-08-15 15:12:20 +0200

    Add files via upload
    Initialize commit

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 // canvas setup
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
-
-canvas.width = 800
+canvas.width = 1440
 canvas.height = 700
-
-
 ctx.fillRect(0, 0, canvas.width, canvas.height)
 
 const gravity = 1
+
 
 //space background
 const background = new Sprite({
@@ -22,8 +20,8 @@ const background = new Sprite({
 // red vortex animation
 var vortex = new Sprite({
     position: {
-        x: 500,
-        y: 200
+        x: 800,
+        y: 300
     },
     imageSrc: './img/vortex.png',
     scale: 2,
@@ -31,9 +29,9 @@ var vortex = new Sprite({
 })
 
 
-var obsticale = new Obstical({
+var obstical = new Obstical({
     position: {
-        x:200,
+        x:500,
         y:200
     },
 
@@ -41,14 +39,35 @@ var obsticale = new Obstical({
         x: 0,
         y: 0
     },
+    imageSrc: './img/sunburn.png',
+    scale: 2,
+    framesMax: 8
+
 })
 
+var panel = new leftPanel ({
+    position: {
+        x: 0,
+        y: 0
+    },
+    imageSrc: './img/leftPanel.png',
+
+})
+
+var panel1 = new rightPanel ({
+    position: {
+        x: 1040,
+        y: 0
+    },
+    imageSrc: './img/rightPanel.png',
+
+})
 
 //player ship
 const player = new Player ({
     position: {
-        x:400,
-        y:400
+        x:500,
+        y:400,
     },
 
     velocity: {
@@ -58,23 +77,40 @@ const player = new Player ({
 })
 player.draw()
 
-// function to draw on canvas
 
+
+ 
+// function to draw on canvas
 function animate() {
     window.requestAnimationFrame(animate)
+
+    
     ctx.fillStyle = "black"
     ctx.fillRect(0, 0, canvas.width, canvas.height) 
     background.update()
     vortex.update()
+    obstical.update()
     player.update()
-    obsticale.update()
+    panel.update()
+    panel1.update()
+
+  
+    if(player.position.x < 400) {
+        player.position.x = player.position.x + 5;
+    }   
     
    
+    if(player.position.y + player.height >= obstical.position.y && player.position.y <= obstical.position.y + obstical.height
+        && player.position.x + player.width >= obstical.position.x && player.position.x <= obstical.position.x + obstical.width) {
+        console.log('true');
+    }
 
    
 }
 
 animate();
+
+
 
 // movement keys
 window.addEventListener('keydown', (event) => {
@@ -118,3 +154,5 @@ window.addEventListener('keyup', (event) => {
     }
 
 })
+
+
--- class.js
+++ class.js
@@ -3,7 +3,7 @@ class Sprite {
     constructor({position, imageSrc, scale = 1, framesMax = 2}) {
         this.position = position
         this.height = 10
-        
+        this.width = 10
         this.image = new Image()
         this.image.src = imageSrc
         this.scale = scale
@@ -49,30 +49,122 @@ class Sprite {
 // Obstical 
 
 class Obstical {
-    constructor({position, velocity}) {
+    constructor({position, velocity, imageSrc, scale = 1, framesMax = 2}) {
         this.position = position
         this.velocity = velocity
         this.height = 100
         this.width = 100
+        this.image = new Image()
+        this.image.src = imageSrc
+        this.scale = scale
+        this.framesMax = framesMax
+        this.framesCurrent = 0
+        this.framesElapsed = 0
+        this.framesHold = 9
     }
 
     draw() {
 
-            ctx.fillStyle = 'red'
-            ctx.fillRect(this.position.x, this.position.y, 50, this.height)
+        ctx.drawImage(
+
+            this.image,
+            this.framesCurrent * (this.image.width / this.framesMax),
+            this.framesCurrent * (this.image.height / this.framesMax),
+            this.image.width / this.framesMax,
+            this.image.height / this.framesMax,
+
+            this.position.x,
+            this.position.y,
+            (this.image.width / this.framesMax) * this.scale,
+            (this.image.height / this.framesMax)* this.scale
+        )
         }
 
     update() {
         this.draw()
+        this.framesElapsed++
 
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-        
-        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
-            this.velocity.y = 0
-        } 
+        if (this.framesElapsed % this.framesHold === 0) {
+
+        if (this.framesCurrent < this.framesMax - 3){
+        this.framesCurrent++
+        } else {
+            this.framesCurrent = 0
+        }
+      }
     }
 }
+
+
+
+class leftPanel {
+    constructor({position, velocity}) {
+        this.position = position
+        this.velocity = velocity
+        this.height = 700
+        this.width = 400
+
+        var image = new Image()
+        image.src = './img/leftPanel.png'
+        this.image = image
+    }
+    
+    draw() {
+        //ctx.fillStyle = 'red'
+        //ctx.fillRect(this.position.x, this.position.y, 10, this.height)
+        ctx.drawImage(
+            this.image,
+            this.position.x,
+            this.position.y,
+            this.width,
+            this.height
+        )
+    }
+
+    update() {
+        this.draw()
+
+        this.position.x 
+        this.position.y 
+        
+        
+    }
+}
+
+class rightPanel {
+    constructor({position, velocity}) {
+        this.position = position
+        this.velocity = velocity
+        this.height = 700
+        this.width = 400
+
+        var image = new Image()
+        image.src = './img/rightPanel.png'
+        this.image = image
+    }
+    
+    draw() {
+        //ctx.fillStyle = 'red'
+        //ctx.fillRect(this.position.x, this.position.y, 10, this.height)
+        ctx.drawImage(
+            this.image,
+            this.position.x,
+            this.position.y,
+            this.width,
+            this.height
+        )
+    }
+
+    update() {
+        this.draw()
+
+        this.position.x 
+        this.position.y 
+        
+        
+    }
+}
+
 
 
 // Player Object
@@ -122,3 +214,5 @@ class Player {
         
     }
 }
+
+
